04ee7ce45f7bac631bf59838af2af7f4841c8823
Matthew John <matthew@dockstudios.co.uk>
2023-08-16 06:10:14 +0100

    Improve layout/comments in example checks

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "jmon_check" "prod_check" {
 
   steps = <<EOF
 - actions:
-   - screenshot: blank
+  - screenshot: blank
 EOF
 }
 
@@ -40,6 +40,7 @@ resource "jmon_check" "api_check" {
   name = "Check_DummyJson_Api"
 
   environment = "default"
+  interval    = 300
 
   steps = <<EOF
 - goto: https://dummyjson.com/products/1
@@ -51,6 +52,10 @@ resource "jmon_check" "api_check" {
     json:
       selector: '$.images[0]'
       contains: 1.jpg
+- check:
+    json:
+      # Check entire response - can be provided as JSON value or YAML
+      equals: {"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]}
 EOF
 }
 
@@ -66,3 +71,4 @@ terraform {
     }
   }
 }
+
